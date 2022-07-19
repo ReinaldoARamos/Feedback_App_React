98d1f1809763091a95c7ffc1c8ac43e43ebc2d76
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-07-19 17:37:16 -0300

    adicionando classes no widget

--- web/src/components/widget.tsx
+++ web/src/components/widget.tsx
@@ -1,5 +1,9 @@
 import {ChatTeardrop} from  'phosphor-react'; 
 
 export function Widget () {
-    return <ChatTeardrop/>
+    return (
+        <button className='bg-violet-500 rounded-full px-3 h-12 text-white'>
+                <ChatTeardrop className='w-6 h-6'/>
+        </button>
+    )
 }
