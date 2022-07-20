0aa2a4a72a26d1c6b7d5de46b2ad53e52f221378
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-07-20 16:51:19 -0300

    colocando uma cor nova e ajustando o tamanho do widget

--- web/src/components/widget.tsx
+++ web/src/components/widget.tsx
@@ -2,8 +2,13 @@ import {ChatTeardrop} from  'phosphor-react';
 
 export function Widget () {
     return (
-        <button className='bg-violet-500 rounded-full px-3 h-12 text-white'>
+        <div className='absolute bottom-4 right-4'>
+            <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center'>
                 <ChatTeardrop className='w-6 h-6'/>
+
+                <span>Feedback</span>
         </button>
+        </div>
+        
     )
 }
