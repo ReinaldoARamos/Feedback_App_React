ec0eab47fba4125a672b58f024386973515dc223
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-07-21 09:11:33 -0300

    Criando o estilo e a animação do botão e dando uma pequena docunmentada de leve

--- web/src/components/widget.tsx
+++ web/src/components/widget.tsx
@@ -2,11 +2,15 @@ import {ChatTeardrop} from  'phosphor-react';
 
 export function Widget () {
     return (
-        <div className='absolute bottom-4 right-4'>
-            <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center'>
+        <div className='absolute bottom-5 right-5'>
+            <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'> 
                 <ChatTeardrop className='w-6 h-6'/>
 
-                <span>Feedback</span>
+        <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+        <span className='pl-2'></span>
+        Feedback
+        </span>
+               
         </button>
         </div>
         
