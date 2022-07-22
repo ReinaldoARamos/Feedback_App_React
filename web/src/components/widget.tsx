499295748269493f6f16602c1d30ae050358a73b
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-07-22 14:58:00 -0300

    Adicionando um state para que quando ele  mude o valor mude também

--- web/src/components/widget.tsx
+++ web/src/components/widget.tsx
@@ -1,5 +1,5 @@
 import {ChatTeardrop} from  'phosphor-react'; 
-import { useCallback, useState } from 'react';
+import {useState } from 'react';
 
 
 export function Widget () {
@@ -8,12 +8,13 @@ export function Widget () {
 
     function ToogleWidgetVisibility(){
 
-        setWidgetOpen(!setWidgetOpen)
+        setWidgetOpen(!isWidgetOpen)
+        console.log(isWidgetOpen)
     }
     return (
         <div className='absolute bottom-5 right-5'>
-            <p>Hello Worlds</p>
-            <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'> 
+          {isWidgetOpen ?   <p>Hello Worlds</p> : null}
+            <button onClick={ToogleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'> 
                 <ChatTeardrop className='w-6 h-6'/>
 
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
