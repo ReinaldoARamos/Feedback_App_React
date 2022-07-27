bf7a5775122fd0c7b50eb5458543d7b69d5b06d6
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-07-27 16:11:21 -0300

    Adicionando headless Ui no react

--- web/src/components/widget.tsx
+++ web/src/components/widget.tsx
@@ -1,21 +1,13 @@
 import {ChatTeardrop} from  'phosphor-react'; 
-import {useState } from 'react';
 import { WidgetForm } from './widgetForm';
-
+import {Popover} from '@headlessui/react';
 
 export function Widget () {
 
-    const [isWidgetOpen, setWidgetOpen] = useState(false) //criando um state, uma variavel que será monitorada e quando mudar muda  o componente
-
-    function ToogleWidgetVisibility(){
-
-        setWidgetOpen(!isWidgetOpen)
-        console.log(isWidgetOpen)
-    }
     return (
-        <div className='absolute bottom-5 right-5 md:bottom-10 md:right-10 flex-col items-end'>
-          {isWidgetOpen ?   <WidgetForm / > : null}
-            <button onClick={ToogleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'> 
+       <Popover className='absolute bottom-4 right-4 md:bottom-10  flex-col items-end'>
+         
+            <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'> 
                 <ChatTeardrop className='w-6 h-6'/>
 
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
@@ -23,8 +15,8 @@ export function Widget () {
         Feedback
         </span>
                
-        </button>
-        </div>
+        </Popover.Button>
+    </Popover>
         
     )
 }
--- web/src/components/widgetForm.tsx
+++ web/src/components/widgetForm.tsx
@@ -1,7 +1,15 @@
 export function WidgetForm() {
     return (
-        <div className="bg-zinc-900 -p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg md: w-auto">
-            Hello World
+        <div className="bg-zinc-900 -p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg ">
+           <header>
+            <span className="text-md leading-5 flex-auto">Deixe o feedback</span>
+           </header>
+
+           
+
+           <footer className="text-md text-neutral-400">
+                    feito com amor pela <a className="underline-offset-2" href="https://www.youtube.com/watch?v=5pCUDdUQspc">Neco Arc :3</a>
+           </footer>
         </div>
-    )
+    );
 }
