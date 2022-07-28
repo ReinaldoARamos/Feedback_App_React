5ca9671ca80ab38b869079cccb3e50f987893d6e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-07-28 16:50:21 -0300

    criando o componente X

--- web/src/components/widgetForm.tsx
+++ web/src/components/widgetForm.tsx
@@ -1,11 +1,16 @@
+import { CloseButton } from "./CloseButton";
 export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
            <header>
             <span className="text-xl leading-6 ">Deixe o feedback</span>
+
+            <CloseButton />
            </header>
 
-           
+           <div className="flex py-8 gap-2">
+
+           </div>
 
            <footer className="text-md text-neutral-400">
                     feito com amor pela <a className="underline underline-offset-2" href="https://www.youtube.com/watch?v=5pCUDdUQspc" target={"_blank"}>Neco Arc :3</a>
