e7aff7fdf5c61c3a5285b21dea90548718ddad68
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-07-28 14:12:56 -0300

    terminando de alinhar o forms

--- web/src/components/widgetForm.tsx
+++ web/src/components/widgetForm.tsx
@@ -2,13 +2,13 @@ export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
            <header>
-            <span className="text-md leading-5 flex-auto">Deixe o feedback</span>
+            <span className="text-xl leading-6 ">Deixe o feedback</span>
            </header>
 
            
 
            <footer className="text-md text-neutral-400">
-                    feito com amor pela <a className="underline-offset-2" href="https://www.youtube.com/watch?v=5pCUDdUQspc">Neco Arc :3</a>
+                    feito com amor pela <a className="underline underline-offset-2" href="https://www.youtube.com/watch?v=5pCUDdUQspc" target={"_blank"}>Neco Arc :3</a>
            </footer>
         </div>
     );
