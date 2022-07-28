7d088c30adb25e4ce5e1effff0f92e0101cac422
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-07-28 16:53:06 -0300

    Criando o componente do botão de fechar e iniciando agora vou começar a criar o painél com os tipos de feedback

--- web/src/components/widgetForm.tsx
+++ web/src/components/widgetForm.tsx
@@ -1,4 +1,11 @@
 import { CloseButton } from "./CloseButton";
+
+const feedbackTypes = {
+    BUG: {},
+    IDEA: {},
+    OTHER: {},
+
+}
 export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
@@ -8,7 +15,9 @@ export function WidgetForm() {
             <CloseButton />
            </header>
 
-           <div className="flex py-8 gap-2">
+           <div className="flex py-8 gap-2 w-full">
+
+            <button></button>
 
            </div>
 
