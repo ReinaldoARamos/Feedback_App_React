73d70b558f1d0f37784a42925f12064d8935124c
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-08-10 15:57:15 -0300

    Adicionando o state para poder definir qual feedback a pessoa tocou baseada no onCLick

--- web/src/components/widgetForm.tsx
+++ web/src/components/widgetForm.tsx
@@ -2,6 +2,7 @@ import { CloseButton } from "./CloseButton";
 import bugImageURL from '../assets/bug.svg';
 import IdeaURL from '../assets/ideia.svg';
 import OtherURL from '../assets/other.svg';
+import { useState } from "react";
 
 
 const feedbackTypes = {
@@ -31,6 +32,7 @@ const feedbackTypes = {
 
 
 export function WidgetForm() {
+    const [FeedbackType, setFeedbackType] = useState(null)
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
            <header>
@@ -45,8 +47,11 @@ export function WidgetForm() {
                 console.log(key, value)
               return (
                 <button
-                className="bg-zinc-800 rounder-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:order-brand-500 focus:outline-none"
-
+                className="bg-zinc-800 rounder-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent
+                          hover:border-brand-500 focus:order-brand-500 focus:outline-none"
+                
+                key={key}
+              //  onClick={}
                 >
                     <img className="h-10"
                      src= {value.Image.source} alt={value.Image.alt}  />
